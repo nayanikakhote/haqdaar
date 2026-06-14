c1a046f15ed87248b42021aff4dcf034370f0f05
Nayanika Khote <nayanikakhote@gmail.com>
2026-06-15 02:36:09 +0530

    pic

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -76,6 +76,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:card", content: "summary" },
     ],
     links: [
+      { rel: "icon", href: "/im.png" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
